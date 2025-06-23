f8533c0de2470a0688af09c2f07481fa686ce3bb
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-06-23 23:07:23 +0200

    update quickhull

--- quickhull/accelerate/src/Main.hs
+++ quickhull/accelerate/src/Main.hs
@@ -12,21 +12,28 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as BI
 import GHC.IsList
 import Data.Int
-import Foreign.ForeignPtr (ForeignPtr, castForeignPtr)
-import qualified Data.Array.Accelerate.IO.Foreign.ForeignPtr as A
+import Foreign.Storable
+import Foreign.ForeignPtr (ForeignPtr, castForeignPtr, withForeignPtr)
+import Foreign.Ptr
 
 main :: IO ()
 main = do
-  inputs <- mapM load ["1M_rectangle", "1M_circle", "1M_quadratic", "100M_rectangle", "100M_circle", "100M_quadratic"]
+  inputs <- mapM load ["25M_rectangle", "25M_circle", "25M_quadratic"]
 
-  let quickhullCPU = CPU.runN quickhull
-  -- let quickhullGPU = GPU.runN quickhull
+  let recursive = quickhullRecursiveThenFlatten CPU.runN
 
-  mapM_ (\input -> mapM_ (`testInput` input) [("CPU", quickhullCPU){-, ("GPU", quickhullGPU) -}]) inputs
+  putStrLn "Recursion depths:"
+  mapM_ (\(name, points) -> putStrLn $ name ++ ": " ++ show (measureRecursionDepth CPU.runN points)) inputs
 
-  defaultMain [backend "CPU" quickhullCPU inputs {-, backend "GPU" quickhullGPU inputs -}]
+  -- mapM_ (\input -> mapM_ (`testInput` input) [("CPU", CPU.runN quickhull1)]) inputs
+
+  -- putStrLn $ A.test @CPU.UniformScheduleFun @CPU.NativeKernel quickhull1
+
+  let variants = [("flat", CPU.runN quickhull1), ("split", CPU.runN quickhull2), ("rec-2", recursive 2), ("rec-5", recursive 5)]
+
+  defaultMain $ map (variant inputs) variants
   where
-    backend name quickhull' inputs
+    variant inputs (name, quickhull')
       = bgroup name
       $ Prelude.map (testcase quickhull') inputs
 
@@ -40,17 +47,16 @@ load name = do
   putStrLn $ "Loading " ++ name
   content <- B.readFile $ "../input/" ++ name ++ ".dat"
   let (fptrw8, nw8) = BI.toForeignPtr0 content
-      arr = A.fromForeignPtrs (A.Z A.:. (nw8 `quot` 4)) (castForeignPtr fptrw8 :: ForeignPtr Int32) :: A.Array (A.Z A.:. Int) Int32
-      res = A.fromFunction (A.Z A.:. (nw8 `quot` 8)) (\(A.Z A.:. ix) -> (fromIntegral $ A.indexArray arr (A.Z A.:. 2*ix), fromIntegral $ A.indexArray arr (A.Z A.:. 2*ix+1))) 
+  res <- withForeignPtr (castForeignPtr fptrw8 :: ForeignPtr Int32) $ \ptr ->
+    A.fromFunctionM (A.Z A.:. (nw8 `quot` 8))
+      (\(A.Z A.:. ix) -> do
+        x <- peekElemOff ptr (2 * ix)
+        y <- peekElemOff ptr (2 * ix + 1)
+        return (fromIntegral x, fromIntegral y))
   return (name, res)
-
 
 testInput :: (String, A.Vector Point -> A.Vector Point) -> Input -> IO ()
 testInput (backend, f) (inputName, inputData) = do
   putStrLn $ backend ++ "/" ++ inputName
   putStrLn $ take 80 $ show $ f inputData
   putStrLn ""
-
-chunk :: Int -> [a] -> [[a]]
-chunk _ [] = []
-chunk i xs = let (f, r) = splitAt i xs in f : chunk i r
--- quickhull/accelerate/src/Quickhull.hs
+++ quickhull/accelerate/src/Quickhull.hs
@@ -1,40 +1,127 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
-module Quickhull (quickhull, Point) where
+module Quickhull where
 
 import Data.Array.Accelerate
-import Data.Array.Accelerate.Debug.Trace
+import qualified Data.Array.Accelerate.Unsafe as Unsafe
 import qualified Prelude
+
+-- Imports for recurive task-parallel implementation. Not used in the fully flattened implementation.
+import Control.Concurrent
+import Control.Exception
+import System.IO.Unsafe
 
 type Point = (Double, Double)
 type Line = (Point, Point)
 type Label = Int
 
-type State =
+-- Flattened state
+type StateFlat =
   ( Vector (Point, Label) -- L, the points that should still be handled, combined with the index of their segment
   , Vector Point -- R, the points that are definitely on the convex hull.
   -- Label i corresponds with the points between R[i] and R[i+1]
   )
 
-quickhull :: Acc (Vector Point) -> Acc (Vector Point)
-quickhull
+-- Non-flattened state
+type State = (Vector Point, Scalar Line)
+
+-- Fully flattened
+quickhull1 :: Acc (Vector Point) -> Acc (Vector Point)
+quickhull1
   = asnd
   -- While there are undecided points,
   . awhile
       (\(T2 points _) -> unit $ size points /= 0)
       -- Add more points to the hull
-      step
+      stepFlat
   -- After creating an initial state, with the input split in two segments
   . initialize
 
-initialize :: Acc (Vector Point) -> Acc State
+-- Mostly flattened. In initializeSplit, we split the instance in two,
+-- which we then both solve (in parallel). Finally we concatenate these two arrays.
+quickhull2 :: Acc (Vector Point) -> Acc (Vector Point)
+quickhull2 input = concatFinal above' below'
+  where
+    T2 above below = initializeSplit input
+    T2 _ above' = awhile (\(T2 points _) -> unit $ size points /= 0) stepFlat $ stepToFlat above
+    T2 _ below' = awhile (\(T2 points _) -> unit $ size points /= 0) stepFlat $ stepToFlat below
+
+quickhullRecursiveThenFlatten
+  :: (forall a b. (Arrays a, Arrays b) => (Acc a -> Acc b) -> (a -> b))
+  -> Int
+  -> Vector Point
+  -> Vector Point
+quickhullRecursiveThenFlatten runN' =
+  -- Make sure that evaluating 'quickhullRecursive runN' to WHNF, also causes
+  -- all Accelerate functions to be compiled.
+  initialize' `Prelude.seq` step' `Prelude.seq` concat1 `Prelude.seq` concat2 `Prelude.seq` flat `Prelude.seq`
+    \maxDepth input -> unsafePerformIO $ do
+      let (a, b) = initialize' input
+      (a', b') <- goParallel maxDepth a b
+      Prelude.return $ concat2 (a', b')
+  where
+    initialize' = runN' initializeSplit
+    step' = runN' step
+    concat1 = runN' $ uncurry concatWithoutOverlap
+    concat2 = runN' $ uncurry concatFinal
+    flat = runN' $ asnd . awhile (\(T2 points _) -> unit $ size points /= 0) stepFlat . stepToFlat
+    
+    goParallel :: Int -> State -> State -> Prelude.IO (Vector Point, Vector Point)
+    goParallel maxDepth state1 state2 = do
+      mvar <- newEmptyMVar
+      _ <- forkIO $ do
+        result1 <- go maxDepth state1 Prelude.>>= evaluate
+        putMVar mvar result1
+      result2 <- go maxDepth state2 Prelude.>>= evaluate
+      result1 <- takeMVar mvar
+      Prelude.return (result1, result2)
+
+    go :: Int -> State -> Prelude.IO (Vector Point)
+    go maxDepth state@(points, line)
+      -- Base case: no more points
+      | arrayShape points Prelude.== (Z :. 0)
+      , (p1, p2) <- indexArray line Z
+      = Prelude.return $ fromList (Z :. 2) [p1, p2]
+      | maxDepth Prelude.== 0
+      = Prelude.return $ flat state
+      | (a, b) <- step' state = do
+        (a', b') <- goParallel (maxDepth - 1) a b
+        Prelude.return $ concat1 (a', b')
+
+measureRecursionDepth
+  :: (forall a b. (Arrays a, Arrays b) => (Acc a -> Acc b) -> (a -> b))
+  -> Vector Point
+  -> Int
+measureRecursionDepth runN' = go . initialize'
+  where
+    go :: StateFlat -> Int
+    go state
+      | indexArray (condition' state) Z = 1 + go (step' state)
+      | otherwise = 0
+
+    initialize' = runN' initialize
+    step' = runN' stepFlat
+    condition' = runN' $ \(T2 points _) -> unit $ size points /= 0
+
+concatWithoutOverlap :: Acc (Vector Point) -> Acc (Vector Point) -> Acc (Vector Point)
+concatWithoutOverlap a b = generate (I1 $ size a + size b - 1) $ \(I1 idx) ->
+  ifThenElse (idx < size a) (a !! idx) (b !! (idx - size a + 1))
+
+concatFinal :: Acc (Vector Point) -> Acc (Vector Point) -> Acc (Vector Point)
+concatFinal a b = generate (I1 $ size a + size b - 2) $ \(I1 idx) ->
+  ifThenElse (idx < size a) (a !! idx) (b !! (idx - size a + 1))
+
+initialize :: Acc (Vector Point) -> Acc StateFlat
 initialize points = T2 newPoints hull
   where
     -- Find the leftmost and rightmost points.
     -- If there are multiple leftmost or rightmost points,
     -- we choose the one with the minimum or maximum y coordinate.
     line@(T2 leftMost rightMost)
-      = the $ fold1 (\(T2 min1 max1) (T2 min2 max2) -> T2 (min min1 min2) (max max1 max2))
+      = the $ fold
+        (\(T2 min1 max1) (T2 min2 max2) -> T2 (minPoint min1 min2) (maxPoint max1 max2))
+        ((T2 (T2 (1.0/0.0) (1.0/0.0)) (T2 (-1.0/0.0) (-1.0/0.0))))
       $ map (\p -> T2 p p) points
     -- Note that this can be done with two folds (one for leftmost and one for rightmost),
     -- but we fused them manually.
@@ -59,7 +146,7 @@ initialize points = T2 newPoints hull
         -- or (0, 0) otherwise.
         f :: Exp Point -> Exp (Int, Int)
         f p
-          = cond (p == leftMost || p == rightMost) (T2 0 0)
+          = cond (p == leftMost ||! p == rightMost) (T2 0 0)
           $ cond (d > 0.0) (T2 1 0)
           $ cond (d < 0.0) (T2 0 1)
             (T2 0 0)
@@ -67,35 +154,52 @@ initialize points = T2 newPoints hull
 
     T2 countUpper countLower = the counts
 
-    -- Compute the destinations for partitioning
-    destination :: Acc (Vector (Maybe DIM1))
-    destination = zipWith f points offsets
-      where
-        f :: Exp Point -> Exp (Int, Int) -> Exp (Maybe DIM1)
-        f p (T2 idxUpper idxLower)
-          = cond (p == leftMost || p == rightMost) Nothing_
-          $ cond (d > 0.0) (Just_ $ I1 idxUpper)
-          $ cond (d < 0.0) (Just_ $ I1 $ countUpper + idxLower)
-            Nothing_
-          where
-            d = nonNormalizedDistance line p
-
     empty :: Acc (Vector (Point, Label))
-    empty = fill (I1 $ countUpper + countLower) (T2 (T2 0.0 0.0) 0)
+    empty = fill (I1 $ countUpper + countLower) Unsafe.undef
 
     -- Perform the actual permutation
     newPoints :: Acc (Vector (Point, Label))
     newPoints
-      = permute const empty (destination !)
-      -- Tuple each point with the index of its segment
-      $ map (\p -> T2 p (nonNormalizedDistance line p < 0.0 ? (1, 0))) points
+      = permuteUnique' empty
+      $ zipWith f points offsets
+      where
+        f :: Exp Point -> Exp (Int, Int) -> Exp (Maybe (DIM1, (Point, Label)))
+        f p (T2 idxUpper idxLower)
+          = cond (p == leftMost ||! p == rightMost) Nothing_
+          $ cond (d > 0.0) (Just_ $ T2 (I1 idxUpper) (T2 p 0))
+          $ cond (d < 0.0) (Just_ $ T2 (I1 $ countUpper + idxLower) (T2 p 1))
+            Nothing_
+          where
+            d = nonNormalizedDistance line p
 
     -- Initial hull consists of only leftMost and rightMost
     hull :: Acc (Vector Point)
     hull = generate (Z_ ::. 2) (\(I1 idx) -> idx == 0 ? (leftMost, rightMost))
 
-step :: Acc State -> Acc State
-step (T2 pointsWithLabels hull) = T2 newPoints newHull
+-- Variant of initialize, that does not flatten the output.
+-- Instead, it returns two StateFlats: one for the part of the input above the
+-- line, one for the part below the line.
+initializeSplit :: Acc (Vector Point) -> Acc (State, State)
+initializeSplit points = T2
+  (T2 above $ unit $ T2 leftMost rightMost)
+  (T2 below $ unit $ T2 rightMost leftMost)
+  where
+    -- Find the leftmost and rightmost points.
+    -- If there are multiple leftmost or rightmost points,
+    -- we choose the one with the minimum or maximum y coordinate.
+    line@(T2 leftMost rightMost)
+      = the $ fold
+        (\(T2 min1 max1) (T2 min2 max2) -> T2 (minPoint min1 min2) (maxPoint max1 max2))
+        ((T2 (T2 (1.0/0.0) (1.0/0.0)) (T2 (-1.0/0.0) (-1.0/0.0))))
+      $ map (\p -> T2 p p) points
+
+    above = afst
+      $ filter (\p -> p /= leftMost &&! p /= rightMost &&! nonNormalizedDistance line p > 0.0) points
+    below = afst
+      $ filter (\p -> p /= leftMost &&! p /= rightMost &&! nonNormalizedDistance line p < 0.0) points
+
+stepFlat :: Acc StateFlat -> Acc StateFlat
+stepFlat (T2 pointsWithLabels hull) = T2 newPoints newHull
   where
     (points, labels) = unzip pointsWithLabels
 
@@ -115,16 +219,17 @@ step (T2 pointsWithLabels hull) = T2 newPoints newHull
 
     -- Store furthest point per segment
     furthestPerLabel :: Acc (Vector Point)
-    furthestPerLabel = permute const
+    furthestPerLabel = permuteUnique'
       -- Use -Infinity as default, to detect empty segments
       -- (parts of the hull where no undecided points remain)
       (generate (shape hull) $ const $ noPoint)
       -- Only write the value if it is the last of a segment
-      (\(I1 idx) ->
-        (idx == size points - 1 || labels !! idx /= labels !! (idx + 1))
-        ? (Just_ $ I1 $ labels !! idx, Nothing_)  
-      )
-      furthest
+      $ imap
+        (\(I1 idx) point ->
+          (idx == size points - 1 || labels !! idx /= labels !! (idx + 1))
+          ? (Just_ $ T2 (I1 $ labels !! idx) point, Nothing_)  
+        )
+        furthest
 
     noPoint = T2 (-1.0/0.0) (-1.0/0.0)
   
@@ -135,17 +240,17 @@ step (T2 pointsWithLabels hull) = T2 newPoints newHull
 
     -- Add furthest points to the hull
     newHull :: Acc (Vector Point)
-    newHull = permute
-      const
-      (permute
-        const
-        (generate (I1 $ the hullNewSize) $ const $ T2 0.0 0.0)
-        (\idx -> Just_ $ I1 $ hullNewIndices ! idx)
-        hull
+    newHull = permuteUnique'
+      (permuteUnique'
+        (generate (I1 $ the hullNewSize) $ const Unsafe.undef)
+        $ imap
+          (\idx point -> Just_ $ T2 (I1 $ hullNewIndices ! idx) point)
+          hull
       )
-      (\idx -> cond (furthestPerLabel ! idx == noPoint) Nothing_
-        $ Just_ $ I1 $ (hullNewIndices ! idx) + 1)
-      furthestPerLabel
+      $ imap
+        (\idx point -> cond (furthestPerLabel ! idx == noPoint) Nothing_
+          $ Just_ $ T2 (I1 $ (hullNewIndices ! idx) + 1) point)
+        furthestPerLabel
 
     -- Filter and reorder the remaining points.
     -- If the corresponding point in the hull of a given segment are A and B,
@@ -161,14 +266,14 @@ step (T2 pointsWithLabels hull) = T2 newPoints newHull
       where
         f :: Exp (Point, Label) -> Exp Int8
         f (T2 p label)
-          = cond (p == f) 0
-          $ cond (nonNormalizedDistance (T2 a f) p > 0.0) 1
-          $ cond (nonNormalizedDistance (T2 f b) p > 0.0) 2
+          = cond (p == furthestPoint) 0
+          $ cond (nonNormalizedDistance (T2 a furthestPoint) p > 0.0) 1
+          $ cond (nonNormalizedDistance (T2 furthestPoint b) p > 0.0) 2
             0
           where
             a = hull !! label
             b = hull !! (label == size hull - 1 ? (0, label + 1))
-            f = furthestPerLabel !! label
+            furthestPoint = furthestPerLabel !! label
 
     -- Compute the destinations for all points.
     -- First compute the local offsets, within a group, within a segment.
@@ -179,23 +284,23 @@ step (T2 pointsWithLabels hull) = T2 newPoints newHull
       $ map (\g -> cond (g == 1) (T2 1 0) $ cond (g == 2) (T2 0 1) (T2 0 0)) groups
 
     segmentSizes :: Acc (Vector (Int, Int))
-    segmentSizes = permute
-      const
-      (generate (shape hull) $ const (T2 0 0))
-      (\(I1 idx) ->
-        (idx == size points - 1 || labels !! idx /= labels !! (idx + 1))
-        ? (Just_ $ I1 $ labels !! idx, Nothing_)  
-      )
-      localOffsets
+    segmentSizes = permuteUnique'
+      (generate (shape hull) $ const $ T2 0 0)
+      $ imap
+        (\(I1 idx) value ->
+          (idx == size points - 1 || labels !! idx /= labels !! (idx + 1))
+          ? (Just_ $ T2 (I1 $ labels !! idx) value, Nothing_)  
+        )
+        localOffsets
     
     segmentOffsets :: Acc (Vector Int)
     newSize :: Acc (Scalar Int)
     T2 segmentOffsets newSize = scanl' (+) 0 $ map (\(T2 size1 size2) -> size1 + size2) segmentSizes
 
-    destination :: Exp DIM1 -> Exp (Maybe DIM1)
-    destination idx
-      = cond (g == 1) (Just_ $ I1 $ segmentOffset + leftOffset - 1)
-      $ cond (g == 2) (Just_ $ I1 $ segmentOffset + leftSize + rightOffset - 1)
+    destination :: Elt a => Exp DIM1 -> Exp a -> Exp (Maybe (DIM1, a))
+    destination idx value
+      = cond (g == 1) (Just_ $ T2 (I1 $ segmentOffset + leftOffset - 1) value)
+      $ cond (g == 2) (Just_ $ T2 (I1 $ segmentOffset + leftSize + rightOffset - 1) value)
       Nothing_
       where
         g = groups ! idx
@@ -209,8 +314,88 @@ step (T2 pointsWithLabels hull) = T2 newPoints newHull
 
     newPoints :: Acc (Vector (Point, Label))
     newPoints
-      = permute const (generate (I1 $ the $ newSize) $ const $ T2 (T2 0.0 0.0) 0) destination
+      = permuteUnique' (generate (I1 $ the newSize) $ const Unsafe.undef)
+      $ imap destination
       $ zipWith setNewLabel pointsWithLabels groups
+
+stepToFlat :: Acc State -> Acc StateFlat
+stepToFlat (T2 points line') = T2 newPoints newHull
+  where
+    line@(T2 p1 p2) = the line'
+
+    -- Use a segmented scan to compute the furthest point to the line of each segment.
+    -- Only the value at the last position of a segment is accurate after this scan. 
+    furthest :: Exp Point
+    furthest
+      = snd -- Drop the distance, only keep the point
+      $ the
+      $ fold max (T2 0 noPoint)
+      $ map (\p -> T2 (nonNormalizedDistance line p) p) points
+
+    noPoint = T2 (1.0/0.0) (1.0/0.0)
+  
+    hullNewSize :: Exp Int
+    hullNewSize = ifThenElse (furthest == noPoint) 2 3
+
+    newHull :: Acc (Vector Point)
+    newHull = generate (Z_ ::. hullNewSize) $ \(I1 idx) ->
+      ifThenElse (idx == 0) p1
+      $ ifThenElse (idx == hullNewSize - 1) p2
+      furthest
+
+    newLabels :: Acc (Vector Int)
+    newLabels = map f points
+      where
+        f :: Exp Point -> Exp Int
+        f p
+          = cond (p == furthest) 2
+          $ cond (nonNormalizedDistance (T2 p1 furthest) p > 0.0) 0
+          $ cond (nonNormalizedDistance (T2 furthest p2) p > 0.0) 1
+            2
+
+    -- Compute the destinations for all points.
+    -- First compute the local offsets, within a group, within a segment.
+    -- Note that this is an inclusive scan, so the offsets are off by one.
+    offsets :: Acc (Vector (Int, Int))
+    segmentSizes :: Acc (Scalar (Int, Int))
+    T2 offsets segmentSizes
+      = scanl' (\(T2 a1 a2) (T2 b1 b2) -> T2 (a1 + b1) (a2 + b2)) (T2 0 0)
+      $ map (\g -> cond (g == 0) (T2 1 0) $ cond (g == 1) (T2 0 1) (T2 0 0)) newLabels
+
+    T2 leftSize rightSize = the segmentSizes
+
+    destination :: Elt a => Exp DIM1 -> Exp a -> Exp (Maybe (DIM1, a))
+    destination idx value
+      = cond (g == 0) (Just_ $ T2 (I1 $ leftOffset) value)
+      $ cond (g == 1) (Just_ $ T2 (I1 $ leftSize + rightOffset) value)
+      Nothing_
+      where
+        g = newLabels ! idx
+        T2 leftOffset rightOffset = offsets ! idx
+
+    newPoints :: Acc (Vector (Point, Label))
+    newPoints
+      = permuteUnique' (generate (I1 $ leftSize + rightSize) $ const Unsafe.undef)
+      $ imap destination $ zip points newLabels
+
+step :: Acc State -> Acc (State, State)
+step (T2 points line') = T2 (T2 left (unit $ T2 p1 furthest)) (T2 right (unit $ T2 furthest p2))
+  where
+    line@(T2 p1 p2) = the line'
+
+    -- Use a segmented scan to compute the furthest point to the line of each segment.
+    -- Only the value at the last position of a segment is accurate after this scan. 
+    furthest :: Exp Point
+    furthest
+      = snd -- Drop the distance, only keep the point
+      $ the
+      $ fold max (T2 0 noPoint)
+      $ map (\p -> T2 (nonNormalizedDistance line p) p) points
+
+    noPoint = T2 (1.0/0.0) (1.0/0.0)
+
+    T2 left _ = filter (\p -> p /= furthest && nonNormalizedDistance (T2 p1 furthest) p > 0.0) points
+    T2 right _ = filter (\p -> p /= furthest && nonNormalizedDistance (T2 furthest p2) p > 0.0) points
 
 -- Labeled inclusive scan.
 segmentedScanl1 :: Elt a => (Exp a -> Exp a -> Exp a) -> Acc (Vector Label) -> Acc (Vector a) -> Acc (Vector a)
@@ -222,13 +407,6 @@ segmentedScanl1 f labels vector
   -- Pair the values with flags denoting whether a segment starts there
   $ imap (\(I1 idx) a -> T2 (idx == 0 || labels !! idx /= labels !! (idx - 1)) a) vector
 
-segmentedScanr1 :: Elt a => (Exp a -> Exp a -> Exp a) -> Acc (Vector Label) -> Acc (Vector a) -> Acc (Vector a)
-segmentedScanr1 f labels vector
-  = map snd
-  $ scanr1 (Prelude.flip (segmented (Prelude.flip f)))
-  -- Pair the values with flags denoting whether a segment ends there
-  $ imap (\(I1 idx) a -> T2 (idx == size vector - 1 || labels !! idx /= labels !! (idx + 1)) a) vector
-
 segmented :: Elt a => (Exp a -> Exp a -> Exp a) -> Exp (Bool, a) -> Exp (Bool, a) -> Exp (Bool, a)
 segmented f (T2 aF aV) (T2 bF bV) = T2 (aF || bF) (bF ? (bV, f aV bV))
 
@@ -239,3 +417,11 @@ nonNormalizedDistance (T2 (T2 x1 y1) (T2 x2 y2)) (T2 x y) = nx * x + ny * y - c
     nx = y1 - y2
     ny = x2 - x1
     c  = nx * x1 + ny * y1
+
+minPoint :: Exp Point -> Exp Point -> Exp Point
+minPoint (T2 x1 y1) (T2 x2 y2) =
+  cond (x1 < x2 ||! (x1 == x2 &&! y1 < y2)) (T2 x1 y1) (T2 x2 y2)
+
+maxPoint :: Exp Point -> Exp Point -> Exp Point
+maxPoint (T2 x1 y1) (T2 x2 y2) =
+  cond (x1 < x2 ||! (x1 == x2 &&! y1 < y2)) (T2 x2 y2) (T2 x1 y1)
