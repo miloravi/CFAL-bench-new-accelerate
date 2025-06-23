5499604aa716285b087c83c95422f8dfd3ffbd0c
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-06-23 14:55:12 +0200

    locvolcalib script

---
+++ LocVolCalib/accelerate/run_old_new.sh
@@ -0,0 +1,63 @@
+TIMER_FALLBACK=""
+DEBUG=""
+for arg in "$@"; do
+  if [[ "$arg" == "--timer-fallback" ]]; then
+    TIMER_FALLBACK="ghc-options:
+  accelerate: -DTRACY_TIMER_FALLBACK"
+  fi
+  if [[ "$arg" == "--debug" && -z "$DEBUG" ]]; then
+    DEBUG="flags:
+  accelerate:
+    debug: true"
+  fi
+  if [[ "$arg" == "--tracy" ]]; then
+    DEBUG="flags:
+  accelerate:
+    debug: true
+    tracy: true"
+  fi
+done
+
+declare -A PKG_NAMES=(
+  [accelerate-llvm-old]="old"
+  [accelerate-llvm]="new"
+)
+
+for pkg in accelerate-llvm-old accelerate-llvm; do
+  name="${PKG_NAMES[$pkg]}"
+  # Remove old results file to avoid appending
+  rm -f "results-$name.csv"
+  # Create a temporary stack.yaml for old and new package
+  cat > temp-stack.yaml <<EOF
+snapshot:
+  url: https://raw.githubusercontent.com/commercialhaskell/stackage-snapshots/master/lts/21/25.yaml
+
+allow-newer: true
+
+packages:
+- .
+- ../../../accelerate
+- ../../../$pkg/accelerate-llvm
+- ../../../$pkg/accelerate-llvm-native
+
+extra-deps:
+- monadLib-3.10.3@sha256:026ba169762e63f0fe5f5c78829808f522a28730043bc3ad9b7c146baedf709f,637
+- github: tomsmeding/llvm-pretty
+  commit: a253a7fc1c62f4825ffce6b2507eebc5dadff32c
+- MIP-0.2.0.0
+- OptDir-0.0.4
+- bytestring-encoding-0.1.2.0
+- acme-missiles-0.3
+- git: https://github.com/commercialhaskell/stack.git
+  commit: e7b331f14bcffb8367cd58fbfc8b40ec7642100a
+- lens-accelerate-0.3.0.0
+$TIMER_FALLBACK
+
+$DEBUG
+EOF
+
+  echo "Running $name"
+  STACK_YAML=temp-stack.yaml stack run locvolcalib -- --csv results-$name.csv
+
+  rm temp-stack.yaml
+done
--- LocVolCalib/accelerate/src/Main.hs
+++ LocVolCalib/accelerate/src/Main.hs
@@ -12,8 +12,8 @@ import qualified Data.Array.Accelerate.Interpreter as Interp
 
 -- currently does NOT work: returns only NaNs. Not sure why, it seems like a faithful translation from Futhark.
 main :: IO ()
-main = print $ Interp.runN main' (fromList Z $ pure $ Prelude.snd $ small) 
-  -- defaultMain [backend "CPU" CPU.runN, backend "GPU" GPU.runN]
+main = --print $ Interp.runN main' (fromList Z $ pure $ Prelude.snd $ small) 
+  defaultMain [backend "CPU" CPU.runN, backend "GPU" GPU.runN]
     where
       backend name run = bgroup name $ map (benchrun run) [small, medium, large]
       benchrun run (name, input) = bench name $ nf (run main') (fromList Z $ pure input)
