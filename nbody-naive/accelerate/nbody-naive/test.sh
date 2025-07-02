7befec6cfa622f67cd0b51e886e65da8875641cc
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-07-02 23:02:31 +0200

    Delete test.sh

--- nbody-naive/accelerate/nbody-naive/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Source shared configuration
-source "../../../accelerate_config.sh" || { echo "Failed to source accelerate_config.sh"; exit 1; }
-
-plot "results/benchmark_CPU_(Scalar_Z_[100000],10).csv"
