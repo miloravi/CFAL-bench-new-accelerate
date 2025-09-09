b46b1b7a193af0448947d56142a29346353041f8
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-09-09 20:07:02 +0200

    add matrix multiplication

--- folds/accelerate/src/Main.hs
+++ folds/accelerate/src/Main.hs
@@ -1,36 +1,65 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 import qualified Data.Array.Accelerate as A
 import qualified Data.Array.Accelerate.LLVM.Native as CPU
 -- import qualified Data.Array.Accelerate.LLVM.PTX    as GPU
 import Control.Monad
-import qualified System.Random.MWC                                  as R
+import Control.DeepSeq
+import qualified System.Random.MWC as R
 import Criterion
 import Criterion.Main
 
+mul2x2Mat :: A.Exp (Int, Int, Int, Int) -> A.Exp (Int, Int, Int, Int) -> A.Exp (Int, Int, Int, Int)
+mul2x2Mat a b = let (a11, a12, a21, a22) = A.unlift a :: (A.Exp Int, A.Exp Int, A.Exp Int, A.Exp Int)
+                    (b11, b12, b21, b22) = A.unlift b :: (A.Exp Int, A.Exp Int, A.Exp Int, A.Exp Int)
+                 in A.lift ( a11 * b11 + a12 * b21
+                           , a11 * b12 + a12 * b22
+                           , a21 * b11 + a22 * b21
+                           , a21 * b12 + a22 * b22
+                           )
+
 main :: IO ()
 main = do 
-  gen <- R.createSystemRandom
-  randomData <- replicateM 100000000 (R.uniformM gen :: IO Int)
-
-  let inputs :: [(String, A.Acc (A.Vector Int))]
+  gen <- R.create
+  randomData1M <- replicateM 1000000 (R.uniformM gen :: IO Int)
+  randomData100M <- replicateM 100000000 (R.uniformM gen :: IO Int)
+  
+  let inputs :: [(String, A.Vector Int)]
       inputs = [
-        ("fromList-0..1000000"  , A.use $ A.fromList (A.Z A.:. 1000000) [0..]),
-        ("generate-0..1000000"  , A.generate (A.I1 1000000) (\(A.I1 i) -> i)),
-        ("fromList-0..100000000", A.use $ A.fromList (A.Z A.:. 100000000) [0..]),
-        ("generate-0..100000000", A.generate (A.I1 100000000) (\(A.I1 i) -> i)),
-        ("random-1000000", A.use $ A.fromList (A.Z A.:. 1000000) randomData),
-        ("random-100000000", A.use $ A.fromList (A.Z A.:. 100000000) randomData)
+        ("sequential-1M", A.fromList (A.Z A.:. 1000000) [0..]),
+        ("sequential-100M", A.fromList (A.Z A.:. 100000000) [0..]),
+        ("random-1M", A.fromList (A.Z A.:. 1000000) randomData1M),
+        ("random-100M", A.fromList (A.Z A.:. 100000000) randomData100M)
         ]
 
+  foldl (\_ (_, v) -> v `deepseq` return ()) (return ()) inputs
+
+  let matrix_inputs :: [(String, A.Vector (Int, Int, Int, Int))]
+      matrix_inputs = map (\(name, vec) ->
+        let A.Z A.:. len = A.arrayShape vec
+            n = len `div` 4
+        in (name, A.fromList (A.Z A.:. n)
+            [ ( A.indexArray vec (A.Z A.:. (i*4 + 0))
+              , A.indexArray vec (A.Z A.:. (i*4 + 1))
+              , A.indexArray vec (A.Z A.:. (i*4 + 2))
+              , A.indexArray vec (A.Z A.:. (i*4 + 3))
+              )
+            | i <- [0..n-1] ])) inputs
+
+  foldl (\_ (_, v) -> v `deepseq` return ()) (return ()) matrix_inputs
+
   defaultMain 
-    [
-      bgroup "sum" $ map (benchFold (+) 0) inputs,
-      bgroup "product" $ map (benchFold (*) 1) inputs,
-      bgroup "max" $ map (benchFold max minBound) inputs 
+    [ bgroup "sum" $ map (benchFold (+) 0) inputs
+    , bgroup "product" $ map (benchFold (*) 1) inputs
+    , bgroup "max" $ map (benchFold max minBound) inputs
+    , bgroup "matrix multiplication (2x2)" $ map (benchFold mul2x2Mat (A.constant (1,0,0,1))) matrix_inputs
     ]
 
-
-benchFold :: (A.Exp Int -> A.Exp Int -> A.Exp Int) -> A.Exp Int -> (String, A.Acc (A.Vector Int)) -> Benchmark
-benchFold f init (name, input) = bench name $ nf (CPU.run . A.fold f init) input
+benchFold :: forall sh a. (A.Shape sh, A.Elt a) => (A.Exp a -> A.Exp a -> A.Exp a) -> A.Exp a -> (String, A.Array (sh A.:. Int) a) -> Benchmark
+benchFold f init (name, input) = bench name $ nfIO $ do
+  return $ CPU.run (A.fold f init (A.use input))
